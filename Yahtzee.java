dc8d024bce6eae90e441fe51b7c7f5e235d0e996
S <s>
2017-02-23 15:52:48 +0800

    {"types":"C","runs":3}

--- Yahtzee.java
+++ Yahtzee.java
@@ -11,29 +11,52 @@ import acm.util.*;
 
 public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	
-	public static void main(String[] args) {
-		new Yahtzee().start(args);
-	}
+    public static void main(String[] args) {
+        new Yahtzee().start(args);
+    }
 	
-	public void run() {
-		IODialog dialog = getDialog();
-		nPlayers = dialog.readInt("Enter number of players");
-		playerNames = new String[nPlayers];
-		for (int i = 1; i <= nPlayers; i++) {
-			playerNames[i - 1] = dialog.readLine("Enter name for player " + i);
-		}
-		display = new YahtzeeDisplay(getGCanvas(), playerNames);
-		playGame();
-	}
 
-	private void playGame() {
-		/* You fill this in */
-	}
+    public void run() {
+        IODialog dialog = getDialog();
+        nPlayers = dialog.readInt("Enter number of players");
+        playerNames = new String[nPlayers];
+        for (int i = 1; i <= nPlayers; i++) {
+            playerNames[i - 1] = dialog.readLine("Enter name for player " + i);
+        }
+        display = new YahtzeeDisplay(getGCanvas(), playerNames);
+        playGame();
+    }
+
+
+    private void playGame() {
+        /* You fill this in */
+        display.waitForPlayerToClickRoll(1);
+        rollDice(dice);
+        display.displayDice(dice);
+
+        
+    }
+
+
+
+
+
+
+
+    /* rolls N_DICE for once */
+    private void rollDice(int[] d) {
+        for (int i = 0; i < N_DICE; i++) {
+            d[i] = rgen.nextInt(1, 6);
+        }
+    }
+
+    
 		
 /* Private instance variables */
-	private int nPlayers;
-	private String[] playerNames;
-	private YahtzeeDisplay display;
-	private RandomGenerator rgen = new RandomGenerator();
+    private int nPlayers;
+    private String[] playerNames;
+    private YahtzeeDisplay display;
+    private RandomGenerator rgen = new RandomGenerator();
+    private int[] dice = new int[N_DICE];
 
 }
