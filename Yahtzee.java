76df7acdb60894407114a16146385e88cf7b1c52
S <s>
2017-03-02 16:42:48 +0800

    {"types":"C","runs":69}

--- Yahtzee.java
+++ Yahtzee.java
@@ -250,7 +250,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
         // }
         //for testing
         d[0] = 6;
-        d[1] = 2;
+        d[1] = 6;
         d[2] = 3;
         d[3] = 4;
         d[4] = 5;
